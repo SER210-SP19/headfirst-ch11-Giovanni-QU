8560df764999ed7601decec713298da366a9ef16
goat <giovanni.greco@quinnipiac.edu>
2019-04-03 17:00:42 -0400

    StopWatch added

--- app/src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
+++ app/src/main/java/edu/quinnipiac/ser210/workout/WorkoutDetailFragment.java
@@ -2,11 +2,10 @@ package edu.quinnipiac.ser210.workout;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-
 import android.widget.TextView;
 
 
@@ -19,7 +18,14 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch);
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        } else {
             workoutId = savedInstanceState.getLong("workoutId");
         }
     }
